f3622e92c4e0fb8b0a816880001fa574724df14b
jpwiig <jpwiig@gmail.com>
2021-12-31 17:58:54 +0100

    litt mer utvidet JS og CSS, samt litt oppdatert CSS, Siste commit i 2021?

--- visuals/Script.js
+++ visuals/Script.js
@@ -1,4 +1,10 @@
 document.getElementById('vinylInfo');
 function retiveInfo(){ 
 vinylInfo.textContent= "Heisann dette er en test";
+setTimeout(retiveInfo, 500); 
+
+}
+
+function showCollection(){
+    vinylInfo.textContent="Your collection: "; 
 }
