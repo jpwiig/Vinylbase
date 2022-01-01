9c90cfa4a9943848ee4fb158e45fc5dcb5550e96
jpwiig <jpwiig@gmail.com>
2022-01-01 14:30:57 +0100

    stuff igjen

--- visuals/Script.js
+++ visuals/Script.js
@@ -1,10 +1,13 @@
 document.getElementById('vinylInfo');
 function retiveInfo(){ 
 vinylInfo.textContent= "Heisann dette er en test";
-setTimeout(retiveInfo, 500); 
 
 }
 
 function showCollection(){
     vinylInfo.textContent="Your collection: "; 
+}
+
+function addRecord(){
+    
 }
