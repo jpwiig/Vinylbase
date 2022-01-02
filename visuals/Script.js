ffbbb30da23f609a9fd31105b2170e1e19512d0d
jpwiig <jpwiig@gmail.com>
2022-01-02 15:32:31 +0100

    ting

--- visuals/Script.js
+++ visuals/Script.js
@@ -7,22 +7,8 @@ vinylInfo.textContent= "Heisann dette er en test";
 function showCollection(){
     vinylInfo.textContent="Your collection: "; 
 }
-
+var Newrec= document.getElementById('nyPlate');
 function addRecord(){
-    vinylInfo.innerHTML=
-        <form>
-            <label>Name of record</label>
-            <br>
-                <input id="Platenavn"></input>
-                <br>
-                    <label>Artist/group</label>
-                    <br>
-                        <input id="artist"></input>
-                        <br>
-                            <label>Release year</label>
-                            <br>
-                                <input id="year"></input>
-                                <br>
-                                    <button>submit</button>
-        </form>
+    vinylInfo.textContent=Newrec;
+
 }
