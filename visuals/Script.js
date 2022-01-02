8f633793186906e9de64203eaa6fae8fa7eed3e1
jpwiig <jpwiig@gmail.com>
2022-01-02 20:16:57 +0100

    figuring out javascript if statements

--- visuals/Script.js
+++ visuals/Script.js
@@ -7,8 +7,22 @@ vinylInfo.textContent= "Heisann dette er en test";
 function showCollection(){
     vinylInfo.textContent="Your collection: "; 
 }
-var Newrec= document.getElementById('nyPlate');
+//if loops in JavaScript
+var vis=document.getElementById('nyPlate');
 function addRecord(){
-    vinylInfo.textContent=Newrec;
-
+  if (vis.style.display==='none'){
+      vis.style.display='block';
+  }
+else{
+    vis.style.display='none';
+  }
+}
+function removeRecord(){
+    vinylInfo.textContent="What record do you want to remove?";
+    if(vinylInfo.style.visibility==="inline"){
+        vinylInfo.style.visibility="none";
+    }
+}
+function addedRecord(){
+    vinylInfo.textContent="Record Added!";
 }
