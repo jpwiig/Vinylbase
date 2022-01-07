c7110d6221b884b8e1d097ebac3041e0b241bd57
jpwiig <jpwiig@gmail.com>
2022-01-07 15:31:44 +0100

    new page alert, and some JSON også

--- visuals/Script.js
+++ visuals/Script.js
@@ -1,58 +1,64 @@
 document.getElementById('vinylInfo');
-var loggInnEpost=document.forms["loggInn"]["email"].value;
-var loggInnPassord=document.forms["loggInn"]["passord"].value;
-function user(){
-if(loggInnEpost==""){
-alert(" please fill in your mail");
-return false;
+
+/*var InnEpost=document.forms["innlogging"]["epost"].value;
+var InnPassord=document.forms["innlogging"]["passord"].value;*/
+var InnEpost = document.forms["innlogging"]["epost"].value;
+var InnPassord = document.forms["innlogging"]["passord"].value;
+function Logginn() {
+
+    if (InnEpost == "") {
+        alert(" please fill in your mail");
+        return false;
+    }
+
+    if (InnPassord == "") {
+        alert("write in your password");
+        return false;
+    }
+    else {
+        alert("All good");
+       top.location="Upage"
+    }
 }
 
-if(loggInnPassord==""){
-alert("write in your password");
-return false;
-}
-else {
-    window.location.href="Upage.html";
-    return false;
-}
+function showCollection() {
+    vinylInfo.textContent = "Your collection: ";
 }
 
-function showCollection(){
-    vinylInfo.textContent="Your collection: ";
-    return false;
-}
 //if loops in JavaScript
- document.getElementById("nyPlate");
-function addRecord(){
+document.getElementById("nyPlate");
+
+function addRecord() {
     //hentet fra: https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
-  if (nyPlate.style.display==="none"){
-      nyPlate.style.display="block";
-  }
-else{
-    nyPlate.style.display="none";
-  }
+    if (nyPlate.style.display === "none") {
+        nyPlate.style.display = "block";
+    } else {
+        nyPlate.style.display = "none";
+    }
 
 }
-function removeRecord(){
-    vinylInfo.textContent="What record do you want to remove?";
+
+function removeRecord() {
+    vinylInfo.textContent = "What record do you want to remove?";
 }
 
 
-
-function addedRecord(){
-    var RecordName=document.forms["new_Record"]["recordName"].value;
-    var Artist=document.forms["new_Record"]["artist"].value;
-    var Release=document.forms["new_Record"]["releaseYear"].value;
-    if (RecordName==""){
+function addedRecord() {
+    var RecordName = document.forms["new_Record"]["recordName"].value;
+    var Artist = document.forms["new_Record"]["artist"].value;
+    var Release = document.forms["new_Record"]["releaseYear"].value;
+    if (RecordName == "") {
         alert("Fill out the name of the  record");
     }
-    if (Artist==""){
+    if (Artist == "") {
         alert("fil out the name of the artist");
     }
-    if(Release==""){
+    if (Release == "") {
         alert("fill out the date");
-    }
-    else{
+    } else {
         alert("cool, sweet, nice");
     }
 }
+$(".gallery").click(function (){
+    $("gallery").slideDown();
+});
